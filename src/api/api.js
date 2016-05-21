3c907789ea8b21cce7f9af27a1755f17928c1298
Jon Sharratt <jon@craftship.io>
2016-05-21 10:17:37 +0100

    Change alerts handler to be async and seperate file

--- src/api/api.js
+++ src/api/api.js
@@ -1,26 +1,13 @@
-import { name, version, description } from './package.json'
+import {
+  name,
+  version,
+  description,
+  author,
+  bugs
+} from './package.json'
+
 import restify from 'restify'
-import RedisSMQ from 'rsmq'
-
-const rsmq = new RedisSMQ({
-  host: 'redis',
-  port: 6379,
-  ns: 'phonebox'
-})
-
-function alertHandler (req, res, next) {
-  rsmq.sendMessage({
-    qname: 'alerts',
-    message: req.body
-  }, (err, id) => {
-    if (err) return next(err)
-
-    if (id) {
-      return res.send(id)
-    }
-    return next()
-  })
-}
+import { alerts } from './handlers'
 
 const server = restify.createServer()
 server.use(restify.bodyParser())
@@ -29,11 +16,13 @@ server.get('/', (req, res) => {
   res.json({
     name,
     version,
-    description
+    description,
+    author,
+    bugs
   })
 })
 
-server.post('/alerts', alertHandler)
+server.post('/alerts', alerts.post)
 
 server.listen(8080, () => {
   console.log('%s listening at %s', server.name, server.url)
