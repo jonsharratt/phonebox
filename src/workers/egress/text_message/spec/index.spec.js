86a1853253170e48c5fe94652a8da6fab134bd51
Jon Sharratt <jon@craftship.io>
2016-05-22 14:44:47 +0100

    Add spec for egress text message

---
+++ src/workers/egress/text_message/spec/index.spec.js
@@ -0,0 +1,82 @@
+import path from 'path'
+import twilio from './mocks/twilio.js'
+import message from './message.json'
+
+function textMessage (scenario) {
+  const TextMessage = injectr('../../egress/text_message/index.js', {
+    'BaseWorker': sinon.stub(),
+    'twilio': twilio[scenario]
+  }, {
+    process,
+    path,
+    __dirname: path.join(__dirname, '../')
+  }).default
+
+  return new TextMessage(sinon.stub())
+}
+
+describe('Text Message', () => {
+  let fixture
+  let next
+
+  beforeEach(() => {
+    fixture = JSON.stringify(message)
+    next = sinon.stub()
+  })
+
+  describe('#body', () => {
+    it('should render in plain text', () => {
+      const subject = textMessage('success')
+      const body = subject.body(fixture)
+
+      assert.equal(body, 'account foo\nbar severity\nfoo bar details\n')
+    })
+  })
+
+  describe('#process', () => {
+    describe('with error', () => {
+      let subject
+
+      beforeEach(() => {
+        subject = textMessage('error')
+        sinon.spy(subject.client, 'sendMessage')
+      })
+
+      it('should pass error to base worker', async (done) => {
+        await subject.process(fixture, next)
+        assert.isFalse(next.calledWith(null))
+        done()
+      })
+
+      afterEach(() => {
+        sinon.restore(subject.client)
+      })
+    })
+
+    describe('without error', () => {
+      let subject
+
+      beforeEach(() => {
+        subject = textMessage('success')
+        sinon.spy(subject.client, 'sendMessage')
+      })
+
+      it('should send a text message', async (done) => {
+        await subject.process(fixture, next)
+        assert.isTrue(subject.client.sendMessage.called)
+        done()
+      })
+
+      it('should delete message from queue', async (done) => {
+        await subject.process(fixture, next)
+        assert.isTrue(next.calledWith(null))
+        done()
+      })
+
+      afterEach(() => {
+        sinon.restore(subject.client)
+      })
+    })
+  })
+})
+
