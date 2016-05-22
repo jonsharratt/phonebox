ebb34da4f2c4fa03e9e72a722ce44a5d7780adc7
Jon Sharratt <jon@craftship.io>
2016-05-22 11:15:13 +0100

    Use twilio promise api instead
    Async awaut all the way!

--- src/workers/egress/text_message/index.js
+++ src/workers/egress/text_message/index.js
@@ -12,18 +12,24 @@ export class TextMessage extends BaseWorker {
     super('text_message', rsmq)
   }
 
-  process (message, next) {
-    client.messages.create({
-      body: super.render(
-        path.join(__dirname, '../templates', 'plain_text.ejs'),
-        message
-      ),
-      to: process.env.TWILIO_TO_NUMBER,
-      from: process.env.TWILIO_FROM_NUMBER
-    }, (err, response) => {
-      if (err) return next(err)
+  body (message) {
+    return this.render(
+      path.join(__dirname, '../templates', 'plain_text.ejs'),
+      message
+    )
+  }
+
+  async process (message, next) {
+    try {
+      await client.sendMessage({
+        body: this.body(message),
+        to: process.env.TWILIO_TO_NUMBER,
+        from: process.env.TWILIO_FROM_NUMBER
+      })
       next()
-    })
+    } catch (err) {
+      next(err)
+    }
   }
 }
 
